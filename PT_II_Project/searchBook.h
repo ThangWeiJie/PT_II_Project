ee80ac98a4a3837d671fea09e5cd36ce0592eb25
Benjamin Chew Jun Jie <jun00@graduate.utm.my>
2024-05-25 22:04:57 +0800

    Add files via upload

--- PT_II_Project/searchBook.h
+++ PT_II_Project/searchBook.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <cmath>
 #include "Book.h"
 using namespace std;
 
@@ -11,13 +12,13 @@ string borderLine(){
     return (string(110,'-')+"\n");
 }
 
-void showSearchResult(vector<Book> result){
+void showSearchResult(vector<Book> result, int p){
     cout << right << setw(50) << "Book List" << endl;
     cout << borderLine();
     cout << right << setw(5) << "No" << " |" << setw(7) << "ID" << setw(6) << "|" << setw(38) << "Title" << setw(33) << "|" << setw(8) << "ISBN" << endl;
     cout << borderLine();
 
-    for(int i=0;i<result.size()&&i<MAX_RESULT_READ;i++){
+    for(int i=0+(p*10);(i<result.size())&&(i<MAX_RESULT_READ+(p*10));i++){
         string titleL0 = result[i].getTitle();
         string titleL1 = " ";
         if(titleL0.length()>70){
@@ -37,17 +38,51 @@ void showSearchResult(vector<Book> result){
 }
 
 void searchBook(vector<Book> books){
-    system("cls");
-    string userSearch = "Pr";
+    string userSearch = "";
+    int userAction = 1, page = 0, totalPage = 0;
+    bool _back = false;
     vector<Book> booksMatched;
 
-    for(int i=0;i<books.size();i++){
-        if(books[i] == userSearch)  booksMatched.push_back(books[i]);
-    }
-    showSearchResult(booksMatched);
-    cout << "Search by Book Title, Author or ISBN: ";
-    cin.ignore();
-    getline(cin,userSearch);
+    do{
+        system("cls");
 
-    system("pause");
+        for(int i=0;i<books.size();i++){
+            if(books[i] == userSearch)  booksMatched.push_back(books[i]);
+        }
+        totalPage = ceil(booksMatched.size()/float(MAX_RESULT_READ));
+        showSearchResult(booksMatched, page);
+
+        cout << "\nSelect action:(1. Search book / 2. Borrow book / ";
+        (page<totalPage-1)? cout << "3. Next page / " : cout << "";
+        (page>0)? cout << "4. Previous page / " : cout << "";
+        cout << "0. Back to user menu) " << endl;
+        cin >> userAction;
+        switch(userAction){
+            case 1:
+                cout << "\nSearch by Book Title, Author or ISBN: ";
+                cin.ignore();
+                getline(cin,userSearch);
+                page = 0;
+                break;
+            case 2:
+                cout << "\nSelect your desired book no.:(1- " << booksMatched.size() << ") ";
+                break;
+            case 3:
+                if(page<totalPage-1) page+=1;
+                break;
+            case 4:
+                if(page>0) page-=1;
+                break;
+            case 0:
+                _back = true;
+                break;
+            default:
+                cout << "Invalid Action Number" << endl;
+                system("pause");
+                break;
+        }
+        
+        booksMatched.clear();
+    }while(!_back);
+        system("pause");
 }
