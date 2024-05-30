2efa86c006fed522aab993446644fb55f055c1a7
Benjamin Chew Jun Jie <jun00@graduate.utm.my>
2024-05-30 09:23:15 +0800

    Add files via upload

---
+++ PT_II_Project/User.h
@@ -0,0 +1,17 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include "Book.h"
+using namespace std;
+
+class User{
+private:
+    string userID, password;
+    bool _isAdmin;
+    Book borrowedBooks;
+public:
+
+}
+
+#endif
